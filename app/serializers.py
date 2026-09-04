94c127c69b89bb7dad1af6f0140a3622464a6532
Maxim <maksimsycev98@gmail.com>
2026-09-04 21:42:48 +0600

    added filtration and pagination

--- app/serializers.py
+++ app/serializers.py
@@ -5,3 +5,8 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+
+class TaskValidateSerializer(serializers.Serializer):
+    name = serializers.CharField(required=True, min_length=2, max_length=255)
+    text = serializers.CharField(required=False, allow_blank=True)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter
 from .models import Task
 from .serializers import TaskSerializer
-# Create your views here.
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    pagination_class = PageNumberPagination
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
